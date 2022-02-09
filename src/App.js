0ec1a706d5b89aa10ff7e4e4ad4b0c5a02082dbb
Swapnil074 <mailswapnil0074@gmail.com>
2022-02-09 18:25:01 +0530

    Website skeleton

--- src/App.js
+++ src/App.js
@@ -24,13 +24,24 @@ const App = () => {
               <Route path="/welcome" component={Homepage} />
               <Route path="/exchanges" component={Exchanges} />
               <Route path="/cryptocurrencies" component={Cryptocurrency} />
-              <Route path="/crypo/:coinId" component={CoinDetails} />
+              <Route path="/crypto/:coinId" component={CoinDetails} />
               <Route path="/news" component={News} />
             </Routes>
           </div>
         </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: "white" }}>
+            CoinGeek <br />
+            All rights reserved
+          </Typography.Title>
+          <Space size="middle" style={{ color: "white" }}>
+            <Link to="/welcome">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/cryptocurrency">Cryptocurrency</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 };
