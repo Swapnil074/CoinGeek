28a34d19981bf21bf9b3de43792238265686ad07
Swapnil074 <mailswapnil0074@gmail.com>
2022-02-13 07:14:23 +0530

    Final Commit

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -35,7 +35,6 @@ const CoinDetails = () => {
 
   const cryptoDetails = data?.data?.coin;
   if (isFetching) return <Loader />;
-  console.log(coinHistory);
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Exchanges = () => {
-  return <div style={{}}>Exchanges</div>;
+  return <div>Exchanges</div>;
 };
 
 export default Exchanges;
